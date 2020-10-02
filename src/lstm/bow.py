6c624fe33768f0f64117afb23648e5887e5db5b3
Vivian <vivianvanoijen@protonmail.com>
2020-10-02 19:17:22 +0200

    Add Dropout

--- src/lstm/bow.py
+++ src/lstm/bow.py
@@ -16,6 +16,8 @@ from sklearn.metrics import classification_report
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as f
 
 # import pdb; pdb.set_trace()
 
@@ -23,18 +25,18 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Data():
     def __init__(self):
-        data = self.read_file(args.in_dir + args.data)
-        labels = self.read_file(args.in_dir + args.labels)
+        self.data = self.read_file(args.in_dir + args.data)
+        self.labels = self.read_file(args.in_dir + args.labels)
 
         fixed_seed = rd.random()
 
-        rd.Random(fixed_seed).shuffle(data)
-        rd.Random(fixed_seed).shuffle(labels)
+        rd.Random(fixed_seed).shuffle(self.data)
+        rd.Random(fixed_seed).shuffle(self.labels)
 
-        data_batches = self.grouper(data, args.batch_size)
-        labels_batches = self.grouper(labels, args.batch_size)
+        data_batches = self.grouper(self.data, args.batch_size)
+        labels_batches = self.grouper(self.labels, args.batch_size)
 
-        if len(data) % args.batch_size != 0:
+        if len(self.data) % args.batch_size != 0:
             data_batches[-1] = [paragraph for paragraph in data_batches[-1]
                                 if paragraph is not None]
             labels_batches[-1] = [label for label in labels_batches[-1]
@@ -50,7 +52,7 @@ class Data():
         self.num_batches = len(self.data_batches)
         self.next_batch = 0
 
-        languages = list(set(labels))
+        languages = list(set(self.labels))
         languages.sort()
 
         self.languages = languages
@@ -134,106 +136,166 @@ class KL(nn.Module):
         self.c3 = 0.58629921
         self.divisor = divisor
 
-    def forward(self, alpha):
-        return (0.5*alpha.log() + self.c1*alpha + self.c2*alpha**2 + self.c3*alpha**3) / self.divisor
+    def forward(self, log_alpha):
+        alpha = torch.exp(log_alpha)
+        divergence = -(0.5*log_alpha + self.c1*alpha + self.c2*alpha**2 + self.c3*alpha**3) / self.divisor
+        return divergence.mean()
 
 class BayesianDropout(nn.Module):
     """
     We follow Max and Kingma paper "Variational Dropout and the Local Reparameterization Trick" (2015)
     Here alpha should be p / (1-p), as shown in the paper.
-
     """
     def __init__(self, dropout_rate, weight_dim, cuda=True):
         super().__init__()
-        self.weight_dim = weight_dim
-        alpha = dropout_rate / (1 - dropout_rate)
-        self.alpha = alpha
-        self.cuda = cuda
-        self._alpha = nn.Parameter(torch.ones(weight_dim)*alpha)
+        self.alpha = dropout_rate / (1 - dropout_rate)
+        self._log_alpha = nn.Parameter(torch.log(torch.ones(weight_dim)*self.alpha), requires_grad=True)
 
-    def forward(self, x):
-        # Unit Gaussian prior
-        noise = torch.autograd.Variable(torch.randn(x.shape)).to(torch.device('cuda' if self.cuda else "cpu"))
+        # init log_alpha params
+        sd = 1. / torch.sqrt(torch.tensor(weight_dim).float())
+        self._log_alpha.data.uniform_(-sd, sd)
+
+    def forward(self, x, eval=False):
+        # Gaussian prior
+        if eval:
+            return x
+        noise = torch.randn(x.shape).to(torch.device('cuda' if self.cuda else "cpu")) + 1
 
         # max=1.0 corresponds with a dropout rate of 0.5 (section 3.3)
-        self._alpha.data = torch.clamp(self._alpha.data, max=1.0)
-        noise *= self._alpha
+        # self._log_alpha.data = torch.clamp(self._log_alpha.data, max=1.0)
+        self._log_alpha.data = self._log_alpha.data.masked_fill(self._log_alpha.data > 1.0, 0)
+        self._log_alpha.data = self._log_alpha.data.masked_fill(self._log_alpha.data < -1.0, 0)
+
+        noise *= torch.exp(self._log_alpha)
         return x * noise
 
-class LinearRegressionModel(torch.nn.Module):
+class LinearRegressionModel(nn.Module):
     def __init__(self, in_features, out_features):
         super(LinearRegressionModel, self).__init__()
         self.linear = nn.Linear(in_features, out_features)
+        self.dropout = BayesianDropout(0.5, out_features)
 
-    def forward(self, x):
-        y_pred = self.linear(x)
-        return y_pred
+    def forward(self, x, eval=False):
+        linear = self.linear(x)
+        prediction = self.dropout(linear, eval)
+        return prediction
 
-class ExtraLinear(torch.nn.Module):
+class ExtraLinear(nn.Module):
     def __init__(self, in_features, out_features):
         super(ExtraLinear, self).__init__()
         self.linear1 = nn.Linear(in_features, 512)
-        # self.dropout = BayesianDropout(0.5, 512)
+        self.dropout = BayesianDropout(0.5, 512)
         self.non_linear = nn.ReLU()
         self.linear2 = nn.Linear(512, out_features)
 
-    def forward(self, x):
+    def forward(self, x, eval=False):
         linear1 = self.linear1(x)
-        # linear1 = self.dropout(linear1)
-        non_linear = self.non_linear(linear1)
-        y_pred = self.linear2(non_linear)
-        return y_pred
+        dropout = self.dropout(linear1, eval)
+        non_linear = self.non_linear(dropout)
+        prediction = self.linear2(non_linear)
+        return prediction
 
 def train(data, model):
     model.train()
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(params=model.parameters())
-    # kl = KL(divisor=50)
+    criterion = nn.CrossEntropyLoss()
+    kl = KL(divisor=2)
+    optimizer = optim.Adam(params=model.parameters())
     for epoch in range(100):
         running_loss = 0.0
+        running_kl = 0.0
         for i in tqdm(range(data.num_batches)):
             batch, labels = data.get_next_embedded_batch()
             batch = torch.from_numpy(batch).float().to(device)
             labels = labels.to(device)
 
             prediction = model.forward(batch)
-            # _alpha = model.dropout._alpha
-            # kl_divergence = torch.sum(kl(_alpha))
-            # print("KL:",kl_divergence.item())
+            log_alpha = model.dropout._log_alpha
+            kl_divergence = kl(log_alpha)
+
+            # if args.model == "linreg":
+            #     log_alpha = model.dropout._log_alpha
+            #     kl_divergence = kl(log_alpha)
+            # else:
+            #     log_alpha1 = model.dropout1._log_alpha
+            #     log_alpha2 = model.dropout2._log_alpha
+            #     kl_divergence = kl(log_alpha1) + kl(log_alpha2)
 
             optimizer.zero_grad()
-            loss = criterion(prediction, labels)
-            # print("CROSS LOSS: ", cross.item())
-            # loss =  cross + kl_divergence
+            cross_entropy = criterion(prediction, labels)
+            loss = cross_entropy + kl_divergence
             loss.backward()
             optimizer.step()
-            # print('epoch {}, it {}, loss {}'.format(epoch, i, loss.item()))
+            # print('epoch {}, it {}, loss {}, kl {}'.format(epoch, i, loss.item(), kl_divergence))
 
             running_loss += loss.item()
+            running_kl += kl_divergence
             if (i + 1) % 300 == 0:
-                print('[epoch %f, iteration %5d] loss: %.3f' %
-                      (int(epoch) + 1, i + 1, running_loss / 300))
+                print('[epoch %f, iteration %5d] loss: %.3f, kl: %.3f' %
+                      (int(epoch) + 1, i + 1, running_loss / 300, running_kl / 300))
                 running_loss = 0.0
         torch.save(model.state_dict(), args.m_dir + args.output + ".pt")
         data.reset_next_batch()
 
-def test(data, model):
+# def test(data, model):
+#     model.eval()
+#     all_predictions = []
+#     all_labels = []
+#     for _ in tqdm(range(data.num_batches)):
+#         batch, labels = data.get_next_embedded_batch()
+#         batch = torch.from_numpy(batch).float().to(device)
+#         labels = labels.to(device)
+#
+#         prediction = model.forward(batch, eval=False)
+#         prediction = torch.argmax(prediction, 1)
+#
+#         all_predictions.extend(prediction.tolist())
+#         all_labels.extend(labels.tolist())
+#     target_names = data.languages
+#     report = classification_report(all_labels, all_predictions,
+#                                  target_names=target_names, output_dict=True)
+#     df = pd.DataFrame(report).transpose()
+#     df.to_csv("classification_report_" + args.output + ".csv", index=True)
+
+def test_uncertainty(data, model):
     model.eval()
-    all_predictions = []
-    all_labels = []
-    for _ in tqdm(range(data.num_batches)):
-        batch, labels = data.get_next_embedded_batch()
-        batch = torch.from_numpy(batch).float().to(device)
-        labels = labels.to(device)
+    y_pred =[]
+    y_true = []
+    accuracies = []
+    for i, datapoint in enumerate(tqdm(data.data)):
+        datapoint = data.bag_of_words([datapoint])
+        datapoint = torch.from_numpy(datapoint).float().to(device)
+        datapoint_probs = torch.zeros(50, len(data.languages))
+        for j in range(50):
+            logits = model(datapoint)
+            probabilities = f.softmax(logits, dim=1)
+            datapoint_probs[j, :] = probabilities
+        standard_deviations = torch.std(datapoint_probs, dim=0)
+        means = torch.mean(datapoint_probs, dim=0)
+        prediction = torch.argmax(means).item()
 
-        prediction = model.forward(batch)
-        prediction = torch.argmax(prediction, 1)
+        label = data.lang_to_idx[data.labels[i]]
 
-        all_predictions.extend(prediction.tolist())
-        all_labels.extend(labels.tolist())
+        y_pred.append(prediction)
+        y_true.append(label)
+
+        correct = (prediction == label)
+        accuracies.append(correct)
+
+        means = [round(mean, 6) for mean in means.cpu().detach().numpy()]
+        std = [round(std_i, 6) for std_i in standard_deviations.cpu().detach().numpy()]
+
+        with open(args.output + ".csv", "a") as file:
+            file.write(str(i)+"; "+ data.idx_to_lang[prediction]+"; " + \
+                        data.idx_to_lang[label]+"; "+str(means)+"; " + \
+                        str(std)+"\n")
+
+    accuracy = round(np.sum(accuracies)/(len(data.data)),4)
+    print("Accuracy: ", accuracy)
     target_names = data.languages
-    return classification_report(all_labels, all_predictions,
-                                 target_names=target_names, output_dict=True)
+    df = pd.DataFrame(classification_report(y_true, y_pred,
+                                            target_names=target_names,
+                                            output_dict=True)).transpose()
+    df.to_csv("classification_report_uncertainty_{}.csv".format(args.output), index=True)
 
 def main():
     data = Data()
@@ -250,9 +312,8 @@ def main():
         train(data, model)
     elif args.mode == "test":
         model.load_state_dict(torch.load(args.m_dir + args.output + ".pt"))
-        report = test(data, model)
-        df = pd.DataFrame(report).transpose()
-        df.to_csv("classification_report_" + args.output + ".csv", index=True)
+        # test(data, model)
+        test_uncertainty(data, model)
     else:
         raise ValueError("Invalid value for argument mode")
 
